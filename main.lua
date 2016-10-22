3aafee95c854e6f4f8d879e4aac7739fc986e3e1
root <root@s2.disrupting.fr>
2016-10-22 13:52:56 -0500

    n

--- main.lua
+++ main.lua
@@ -3,7 +3,6 @@ require 'nngraph'
 require 'cutorch'
 require 'cunn'
 require 'optim'
-local model_utils = require 'model_utils'
 
 cmd = torch.CmdLine()
 cmd:text()
@@ -11,20 +10,20 @@ cmd:text()
 cmd:text('Training a simple grid lstm 1D network')
 cmd:text()
 cmd:text('Options')
-cmd:option('-hidden',1500,'number of hidden neurons')
-cmd:option('-layers',72,'number of layers')
-cmd:option('-learning_rate',0.06,'learning rate')
-cmd:option('-batch_size',20,'batch size')
+cmd:option('-hidden',500,'number of hidden neurons')
+cmd:option('-layers',5,'number of layers')
+cmd:option('-learning_rate',0.005,'learning rate')
+cmd:option('-batch_size',100,'batch size')
 cmd:option('-iterations',15000000,'nb of iterations')
-cmd:option('-nb_bits',220,'input number of bits')
+cmd:option('-nb_bits',10,'input number of bits')
 cmd:option('-eval_interval',1000,'print interval')
+cmd:option('-untied_weights',false,'Untie LSTM if true')
 cmd:option('-gpu',1,'gpu to use. if 0, cpu')
-cmd:option('-grad_clip',5,'clip gradients at this value')
 cmd:text()
-opt = cmd:parse(arg)
+params = cmd:parse(arg)
 
 -- gpu activation
-local gpu = opt.gpu > 0
+local gpu = params.gpu > 0
 if gpu then
   ok, cunn = pcall(require, 'cunn')
   ok2, cutorch = pcall(require, 'cutorch')
@@ -32,24 +31,24 @@ if gpu then
 end
 
 if gpu then
-  print("Using GPU", opt.gpu)
-  cutorch.setDevice(opt.gpu)
+  print("Using GPU", params.gpu)
+  cutorch.setDevice(params.gpu)
 else
   print("GPU OFF")
 end
 
 -- build model
-cell_dim = opt.hidden
+cell_dim = params.hidden
 
 inputs = {}
 table.insert(inputs,nn.Identity()())
-m0 = inputs[1]
-h0 = inputs[1]
+m0 = nn.Linear(params.nb_bits, cell_dim)(inputs[1])
+h0 = nn.Linear(params.nb_bits, params.hidden)(inputs[1])
 
-forget_lin = nn.Linear(opt.hidden,cell_dim)
-input_lin = nn.Linear(opt.hidden,cell_dim)
-candidate_lin = nn.Linear(opt.hidden,cell_dim)
-output_lin = nn.Linear(opt.hidden,cell_dim)
+forget_lin = nn.Linear(params.hidden,cell_dim)
+input_lin = nn.Linear(params.hidden,cell_dim)
+candidate_lin = nn.Linear(params.hidden,cell_dim)
+output_lin = nn.Linear(params.hidden,cell_dim)
 
 forget_gate_ = nn.Sigmoid()(forget_lin(h0))
 input_gate_ = nn.Sigmoid()(input_lin(h0))
@@ -59,21 +58,21 @@ output_gate_ = nn.Sigmoid()(output_lin(h0))
 m = nn.CAddTable()( { nn.CMulTable()({input_gate_,candidate_}),nn.CMulTable()({forget_gate_,m0}) })
 h = nn.Tanh()( nn.CMulTable()({ output_gate_, m }) )
 
-for n=2,opt.layers do
-  forget_lin_ = nn.Linear(opt.hidden,cell_dim)
-  forget_lin_:share( forget_lin, 'weight', 'bias','gradWeight', 'gradBias'  )
+for n=2,params.layers do
+  forget_lin_ = nn.Linear(params.hidden,cell_dim)
+  if not params.untied_weights then forget_lin_:share( forget_lin, 'weight', 'bias','gradWeight', 'gradBias'  ) end
   forget_gate_ = nn.Sigmoid()(forget_lin_(h))
 
-  input_lin_ = nn.Linear(opt.hidden,cell_dim)
-  input_lin_:share( input_lin, 'weight', 'bias','gradWeight', 'gradBias'  )
+  input_lin_ = nn.Linear(params.hidden,cell_dim)
+  if not params.untied_weights then input_lin_:share( input_lin, 'weight', 'bias','gradWeight', 'gradBias'  ) end
   input_gate_ = nn.Sigmoid()(input_lin_(h))
 
-  candidate_lin_ = nn.Linear(opt.hidden,cell_dim)
-  candidate_lin_:share( candidate_lin, 'weight', 'bias','gradWeight', 'gradBias'  )
+  candidate_lin_ = nn.Linear(params.hidden,cell_dim)
+  if not params.untied_weights then candidate_lin_:share( candidate_lin, 'weight', 'bias','gradWeight', 'gradBias'  ) end
   candidate_ = nn.Tanh()(candidate_lin_(h))
 
-  output_lin_ = nn.Linear(opt.hidden,cell_dim)
-  output_lin_:share( output_lin, 'weight', 'bias','gradWeight', 'gradBias'  )
+  output_lin_ = nn.Linear(params.hidden,cell_dim)
+  if not params.untied_weights then output_lin_:share( output_lin, 'weight', 'bias','gradWeight', 'gradBias'  ) end
   output_gate_ = nn.Sigmoid()(output_lin_(h))
 
   m = nn.CAddTable()( { nn.CMulTable()({input_gate_,candidate_}),nn.CMulTable()({forget_gate_,m}) })
@@ -81,72 +80,67 @@ for n=2,opt.layers do
 end
 
 outputs = {}
-table.insert(outputs, nn.Sigmoid()( nn.Linear( cell_dim + opt.hidden , 1)( nn.JoinTable(2)({h,m}) )))
+table.insert(outputs, nn.LogSoftMax()(nn.Linear( cell_dim + params.hidden , 2)( nn.JoinTable(2)({h,m}) )))
 
 mlp = nn.gModule(inputs, outputs)
+---------Functions:-----------
+validation_losses,validation_accuracies = {},{}
 
--- put the above things into one flattened parameters tensor
-params, grad_params = model_utils.combine_all_parameters(mlp)
-
--- initialization
-if do_random_init then
-  params:uniform(-0.08, 0.08) -- small uniform numbers
-end
-
-criterion = nn.BCECriterion()
-if gpu then
-  mlp = mlp:cuda()
-  criterion=criterion:cuda()
-end
-
--- generate data
-function next_batch()
-  input = torch.zeros(opt.batch_size,opt.hidden)
-  input[{{},{1,opt.nb_bits}}] = torch.rand(opt.batch_size,opt.nb_bits):round()
-  target = (input:sum(2) % 2):squeeze()
+function generate_data()
+  local input = torch.rand(params.batch_size,params.nb_bits):round()
+  local target = (input:sum(2) % 2 +1):squeeze()
   if gpu then
-     input=input:cuda()
-     target = target:cuda()
+    input=input:cuda()
+    target = target:cuda()
   end
-  return input, target
+    return input,target
 end
 
-function feval(x)
-  if x ~= params then
-    params:copy(x)
-  end
-  grad_params:zero()
-
-  local input, target = next_batch()
+function train() 
+  mlp:zeroGradParameters()
+  local input,target = generate_data()
   local out = mlp:forward(input)
   local loss = criterion:forward(out, target)
   local gradOut = criterion:backward(out, target)
   mlp:backward(input, gradOut)
-  grad_params:clamp(-opt.grad_clip, opt.grad_clip)
-  return loss, grad_params
+  --mlp:updateParameters(params.learning_rate) 
+   return loss, gradParameters 
 end
 
-function eval()
-  local input, target = next_batch()
-  local out = mlp:forward(input)
-  local loss = criterion:forward(out, target)
-  return out,loss
+function evaluation()
+	local loss, accuracy = 0, 0
+	local n = math.floor(100/params.batch_size)
+	for i=1,n do
+		local input,target = generate_data()
+		local out = mlp:forward(input)
+		loss = loss + criterion:forward(out, target) 
+ 		accuracy = accuracy + (out[{{},1}]:exp():round() - target + 1):abs():mean() 
+	end
+  table.insert(validation_losses, loss / n)
+  table.insert(validation_accuracies,accuracy / n)
+  return loss /n, accuracy / n
 end
 
 ---------Training:------------
-training_losses,accuracies = {},{}
-local optim_state = {learningRate = opt.learning_rate}
-for e=1,opt.iterations/opt.batch_size do
-   optim.adagrad(feval, params, optim_state)
-   if e % opt.eval_interval == 0 then
-      local out,loss = eval()
-      table.insert(training_losses, loss)
-      accuracy = (out:round()-target):abs():mean()
-      table.insert(accuracies,accuracy)
-      print("Iteration", e,"Training Loss",loss,"Accuracy",accuracy)
-   end
+criterion = nn.ClassNLLCriterion()
+if gpu then
+  mlp = mlp:cuda()
+  criterion=criterion:cuda()
+end
+parameters, gradParameters = mlp:getParameters()
+--parameters:uniform(-0.005, 0.005)
+config = {
+   learningRate = params.learning_rate,
+}
+
+for e=1,params.iterations / params.batch_size do
+  optim.adagrad(train, parameters, config)
+  if e % params.eval_interval == 0 then
+    local validation_loss,validation_accuracy = evaluation()
+    print("Iteration", e*params.batch_size,"Loss",validation_loss*100,"Accuracy",validation_accuracy*100)
+  end
 end
 
----------Saving:------------
-torch.save(string.format( "shared_w_accuracies_i%s_lr%s_h%s_lay%s_b%s", opt.iterations, opt.learning_rate, opt.hidden, opt.layers, opt.batch_size), accuracies)
-torch.save(string.format( "shared_w_losses_i%s_lr%s_h%s_lay%s_b%s", opt.iterations, opt.learning_rate, opt.hidden, opt.layers, opt.batch_size), training_losses)
+
+torch.save(string.format( "shared_w_accuracies_i%s_lr%s_h%s_lay%s_b%s_nbb%s", params.iterations, params.learning_rate, params.hidden, params.layers, params.batch_size, params.nb_bits), validation_accuracies)
+torch.save(string.format( "shared_w_losses_i%s_lr%s_h%s_lay%s_b%s_nbb%s", params.iterations, params.learning_rate, params.hidden, params.layers, params.batch_size, params.nb_bits), validation_losses)
